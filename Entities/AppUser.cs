62329e7aac3fe77a8684286609833cebfa2a0366
nassar <nassarr812@gmail.com>
2024-03-04 22:40:06 +0300

    end of sec 14

--- Entities/AppUser.cs
+++ Entities/AppUser.cs
@@ -18,4 +18,7 @@ public class AppUser
     public string Country { get; set; }
     public List<Photo> Photos { get; set; } = new();
 
+    public List<UserLike> LikedByUsers { get; set; }
+    public List<UserLike> LikedUsers { get; set; }
+
 }
